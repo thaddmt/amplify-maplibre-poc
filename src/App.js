1bf607332cbeb6504d31b71414638784c16e63cf
Praveen Gupta <pravgupt@amazon.com>
2021-04-16 17:07:04 -0700

    POC First cut

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import Amplify, { Auth } from "aws-amplify";
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
+import awsconfig from "./aws-exports";
+
+// New MapLibre Plugin
+import SearchControl from "./MapLibrePlugin/SearchControl";
+import MapBoxRequest from "./MapLibrePlugin/MapBoxRequest";
+import GeofenceControl from "./MapLibrePlugin/GeofenceControl";
+
+// New Amplify Plugin
+import { Geo } from "./AmplifyGeoPlugin/AmplifyGeo";
+import { Map, NavigationControl } from "maplibre-gl";
+import "./App.css";
+
+Amplify.configure(awsconfig);
+
+async function initializeMap() {
+  // actually initialize the map
+  const map = new Map({
+    container: "map",
+    center: [-123.1187, 49.2819], // initial map center point
+    zoom: 10, // initial map zoom
+    style: "test-maps-1",
+    transformRequest: new MapBoxRequest(await Auth.currentCredentials())
+      .transformRequest,
+  });
+
+  map.addControl(new NavigationControl(), "top-left");
+  map.addControl(
+    new SearchControl({ placeIndexResource: "test-places-1", api: Geo }),
+    "top-right"
+  );
+  map.addControl(
+    new GeofenceControl({
+      geofenceCollectionResource: "test-geofence-collection-1",
+      api: Geo,
+    }),
+    "bottom-left"
+  );
+}
 
 function App() {
+  initializeMap();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AmplifyAuthenticator>
+      <div id="map" />
+    </AmplifyAuthenticator>
   );
 }
 
